9baf46bf6e7f50e77ee06f5bdef30e1f07c81d80
FugaTakata <maple1213goo@gmail.com>
2021-06-12 08:54:17 +0900

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,23 +3,38 @@ import seed from "./seed-data/jfdb_after2003.json";
 
 const prisma = new PrismaClient();
 
-export const main = async () => {
+const main = async () => {
   const { data } = seed as any;
 
   for (const d of data) {
     const movie = await prisma.movie.create({
       data: {
         title: d.タイトル,
-        releaseDate: new Date(d.公開年月日),
+        releaseDate: new Date(
+          d.公開年月日.replace("年", "-").replace("月", "-").replace("日", "")
+        ),
         story: d.説明,
         MoviesOnActors: {
           create: d.出演者.map((c) => {
             return {
-              data: {
-                name: c.名前,
+              cast: {
+                create: {
+                  name: c.名前,
+                },
               },
             };
           }),
+          // create: {
+          //   cast: {
+          //     create: d.出演者.map((c) => {
+          //       return {
+          //         data: {
+          //           name: c.名前,
+          //         },
+          //       };
+          //     }),
+          //   },
+          // },
         },
       },
     });
@@ -39,6 +54,6 @@ main()
     console.error(e);
     process.exit(1);
   })
-  .finally(() => {
-    prisma.$disconnect();
+  .finally(async () => {
+    await prisma.$disconnect();
   });
