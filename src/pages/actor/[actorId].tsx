42273ce9faee4a6ae600d502871a5db9cadf5828
FugaTakata <maple1213goo@gmail.com>
2021-06-14 21:26:40 +0900

    init page

---
+++ src/pages/actor/[actorId].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActorDetails: React.VFC = () => {
+  return <h1 className="title">ActorDetails</h1>;
+};
+
+export default ActorDetails;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <h1 className="title">Hello World!</h1>;
-}
+import React from "react";
+
+const Home: React.VFC = () => {
+  return <h1 className="title">Home</h1>;
+};
+
+export default Home;
