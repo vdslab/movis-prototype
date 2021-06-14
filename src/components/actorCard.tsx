82723df55321276c64ac5536a486828c2308f6a2
FugaTakata <maple1213goo@gmail.com>
2021-06-15 00:08:30 +0900

    add card and tmdb api

---
+++ src/components/actorCard.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { TMDB_API_KEY } from "~/env/tmdb";
+
+interface ActorCardProps {
+  name: string;
+  id: string;
+}
+
+const apiBaseUrl = "https://api.themoviedb.org/3/search/person";
+const imgBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+const ActorCard: React.VFC<ActorCardProps> = ({ name, id }) => {
+  const [imageSrc, setImageSrc] = useState("/noImage.jpeg");
+  useEffect(() => {
+    (async () => {
+      const params = new URLSearchParams();
+      params.set("api_key", TMDB_API_KEY);
+      params.set("language", "ja-JP");
+      params.set("query", name);
+      params.set("page", "1");
+      params.set("include_adult", "false");
+      params.set("region", "JP");
+
+      const res = await fetch(`${apiBaseUrl}?${params.toString()}`);
+      const data = await res.json();
+
+      if (data.results.length !== 0 && data.results[0].profile_path !== null) {
+        setImageSrc(`${imgBaseUrl}${data.results[0].profile_path}`);
+      }
+    })();
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className={"image is-2by3"}>
+          <img width="622px" height="933px" src={imageSrc} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="content">
+          <p>{name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActorCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,96 @@
-import React from "react";
+import { Actor } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import ActorCard from "~/components/actorCard";
+import Layout from "~/components/layout";
 
 const Home: React.VFC = () => {
-  return <h1 className="title">Home</h1>;
+  const router = useRouter();
+  const [input, setInput] = useState({ name: "" });
+  const [actors, setActors] = useState<Actor[]>([]);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setInput((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const params = new URLSearchParams();
+      params.set("page", "0");
+      const res = await fetch(`/api/actors?${params.toString()}`);
+      const data: Actor[] = await res.json();
+      console.log(data);
+
+      setActors(data);
+    })();
+  }, []);
+
+  return (
+    <Layout>
+      <section className="section">
+        <div className="columns">
+          <div className="column is-one-quarter">
+            <div className="box">
+              <form>
+                <label className="label">名前</label>
+                <div className="field has-addons">
+                  <div className="control">
+                    <input
+                      name="name"
+                      className="input"
+                      type="text"
+                      value={input.name}
+                      onChange={handleChangeInput}
+                    />
+                  </div>
+                  <div className="control">
+                    <button className="button is-link">検索</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="box">
+              {actors
+                .reduce(
+                  (a, c, i) =>
+                    i % 4 == 0
+                      ? [...a, [c]]
+                      : [...a.slice(0, -1), [...a[a.length - 1], c]],
+                  []
+                )
+                .map((array, i) => {
+                  console.log(array);
+                  return (
+                    <div className="columns" key={i}>
+                      {array.map((actor) => {
+                        return (
+                          <Link href={`/movie/${actor.id}`} key={actor.id}>
+                            <a className="column is-3">
+                              <ActorCard name={actor.name} id={actor.id} />
+                            </a>
+                          </Link>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Home;
