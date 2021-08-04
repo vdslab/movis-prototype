f6bd6e767085fcfc3a9ffc39e94207fe21460788
yu2799 <ura.yu.2799@gmail.com>
2021-08-04 17:46:08 +0900

    format

--- src/components/revenue.tsx
+++ src/components/revenue.tsx
@@ -21,35 +21,35 @@ export const Revenue = ({ data }: { data: Movie[] }) => {
     }),
   };
   return (
-        <ResponsiveCirclePacking
-          data={graphData}
-          margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-          id={"title"}
-          value={"revenue"}
-          valueFormat={" >-,~"}
-          colors={{ scheme: "paired" }}
-          childColor={{ from: "color", modifiers: [["brighter", 0.4]] }}
-          padding={4}
-          enableLabels={true}
-          labelsFilter={function (e) {
-            return 2 === e.node.depth;
-          }}
-          labelsSkipRadius={11}
-          labelTextColor={{ from: "color", modifiers: [["darker", 3]] }}
-          borderWidth={1}
-          borderColor={{ from: "color", modifiers: [["darker", 0.5]] }}
-          defs={[
-            {
-              id: "lines",
-              type: "patternLines",
-              background: "none",
-              color: "inherit",
-              rotation: -45,
-              lineWidth: 5,
-              spacing: 8,
-            },
-          ]}
-          onClick={(e) => console.log(e)}
-        />
+    <ResponsiveCirclePacking
+      data={graphData}
+      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+      id={"title"}
+      value={"revenue"}
+      valueFormat={" >-,~"}
+      colors={{ scheme: "paired" }}
+      childColor={{ from: "color", modifiers: [["brighter", 0.4]] }}
+      padding={4}
+      enableLabels={true}
+      labelsFilter={function (e) {
+        return 2 === e.node.depth;
+      }}
+      labelsSkipRadius={11}
+      labelTextColor={{ from: "color", modifiers: [["darker", 3]] }}
+      borderWidth={1}
+      borderColor={{ from: "color", modifiers: [["darker", 0.5]] }}
+      defs={[
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "none",
+          color: "inherit",
+          rotation: -45,
+          lineWidth: 5,
+          spacing: 8,
+        },
+      ]}
+      onClick={(e) => console.log(e)}
+    />
   );
 };
