1db36c5b1adc43550e75923715f829c87bf322b1
FugaTakata <maple1213goo@gmail.com>
2021-08-04 18:09:00 +0900

    update revenue graph

--- src/components/revenue.tsx
+++ src/components/revenue.tsx
@@ -21,35 +21,52 @@ export const Revenue = ({ data }: { data: Movie[] }) => {
     }),
   };
   return (
-    <ResponsiveCirclePacking
-      data={graphData}
-      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-      id={"title"}
-      value={"revenue"}
-      valueFormat={" >-,~"}
-      colors={{ scheme: "paired" }}
-      childColor={{ from: "color", modifiers: [["brighter", 0.4]] }}
-      padding={4}
-      enableLabels={true}
-      labelsFilter={function (e) {
-        return 2 === e.node.depth;
-      }}
-      labelsSkipRadius={11}
-      labelTextColor={{ from: "color", modifiers: [["darker", 3]] }}
-      borderWidth={1}
-      borderColor={{ from: "color", modifiers: [["darker", 0.5]] }}
-      defs={[
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "none",
-          color: "inherit",
-          rotation: -45,
-          lineWidth: 5,
-          spacing: 8,
-        },
-      ]}
-      onClick={(e) => console.log(e)}
-    />
+    <div className="">
+      <div
+        className="columns"
+        style={{
+          overflowX: "auto",
+          whiteSpace: "nowrap",
+        }}
+      >
+        {graphData.children.map((d) => {
+          return (
+            <div
+              className="column is-4 is-text-center is-inline-block"
+              style={{ height: "600px" }}
+            >
+              <h2 className="subtitle">{d.title}</h2>
+              <ResponsiveCirclePacking
+                data={d}
+                margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+                id={"title"}
+                value={"revenue"}
+                valueFormat={" >-,~"}
+                colors={{ scheme: "paired" }}
+                childColor={{ from: "color", modifiers: [["brighter", 0.4]] }}
+                padding={4}
+                enableLabels={true}
+                labelsSkipRadius={0}
+                labelTextColor={{ from: "color", modifiers: [["darker", 3]] }}
+                borderWidth={1}
+                borderColor={{ from: "color", modifiers: [["darker", 0.5]] }}
+                defs={[
+                  {
+                    id: "lines",
+                    type: "patternLines",
+                    background: "none",
+                    color: "inherit",
+                    rotation: -45,
+                    lineWidth: 5,
+                    spacing: 8,
+                  },
+                ]}
+                onClick={(e) => console.log(e)}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
